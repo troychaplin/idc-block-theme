22ecbd472972b8553053721838868622a7d630a1
Troy Chaplin <troy.chaplin@gmail.com>
2026-05-08 22:41:16 -0400

    update header

--- patterns/header-en.php
+++ patterns/header-en.php
@@ -6,22 +6,28 @@
  */
 ?>
 
-<!-- wp:group {"metadata":{"name":"Site Header"},"className":"idc-site-header","style":{"position":{"type":""},"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}},"border":{"top":{"color":"var:preset|color|primary-dark","width":"5px"},"right":[],"bottom":{"color":"var:preset|color|custom-white-10","width":"1px"},"left":[]}},"backgroundColor":"primary","layout":{"type":"constrained","contentSize":"1280px"},"anchor":"site-header"} -->
-<div id="site-header" class="wp-block-group idc-site-header has-primary-background-color has-background" style="border-top-color:var(--wp--preset--color--primary-dark);border-top-width:5px;border-bottom-color:var(--wp--preset--color--custom-white-10);border-bottom-width:1px;padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:group {"tagName":"section","metadata":{"name":"Section"},"className":"idc-site-header__section","style":{"border":{"bottom":{"width":"0px","style":"none"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<section class="wp-block-group idc-site-header__section" style="border-bottom-style:none;border-bottom-width:0px"><!-- wp:group {"tagName":"header","metadata":{"name":"IDC Logo"},"className":"idc-site-header__logo","style":{"spacing":{"blockGap":"var:preset|spacing|30"},"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<header class="wp-block-group idc-site-header__logo"><!-- wp:site-logo {"width":38,"isLink":false,"className":"is-style-default","style":{"layout":{"selfStretch":"fit","flexSize":null}}} /-->
+<!-- wp:group {"metadata":{"name":"Site Header"},"className":"idc-site-header","style":{"position":{"type":""},"spacing":{"padding":{"top":"var:preset|spacing|40"}},"border":{"bottom":{"color":"var:preset|color|custom-white-5","width":"1px"},"top":{"color":"var:preset|color|primary-dark","width":"5px"},"right":[],"left":[]}},"backgroundColor":"primary","layout":{"type":"constrained","contentSize":"1280px"},"anchor":"site-header"} -->
+<div id="site-header" class="wp-block-group idc-site-header has-primary-background-color has-background" style="border-top-color:var(--wp--preset--color--primary-dark);border-top-width:5px;border-bottom-color:var(--wp--preset--color--custom-white-5);border-bottom-width:1px;padding-top:var(--wp--preset--spacing--40)"><!-- wp:group {"tagName":"section","metadata":{"name":"Section"},"className":"idc-site-header__section","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<section class="wp-block-group idc-site-header__section"><!-- wp:group {"tagName":"header","metadata":{"name":"IDC Logo"},"className":"idc-site-header__logo","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<header class="wp-block-group idc-site-header__logo"><!-- wp:image {"className":"idc-site-logo"} -->
+<figure class="wp-block-image idc-site-logo"><img src="/wp-content/themes/idc-block-theme/assets/images/idc-logo-white.png" alt="International Documents Canada"/></figure>
+<!-- /wp:image -->
 
-<!-- wp:site-title {"level":0,"className":"idc-site-title","style":{"border":{"radius":"0px","width":"0px","style":"none"},"spacing":{"padding":{"top":"0","bottom":"0","left":"0rem","right":"0rem"}},"elements":{"link":{"color":{"text":"var:preset|color|white"},":hover":{"color":{"text":"var:preset|color|primary-pale"}}}},"layout":{"selfStretch":"fixed","flexSize":"100px"},"typography":{"lineHeight":"1.2","fontSize":"0.85rem","fontStyle":"normal","fontWeight":"500"}},"textColor":"white"} /--></header>
+<!-- wp:site-title {"level":0,"className":"idc-site-title","style":{"border":{"radius":"0px","width":"0px","style":"none"},"elements":{"link":{"color":{"text":"var:preset|color|white"},":hover":{"color":{"text":"var:preset|color|primary-pale"}}}}},"textColor":"white"} /--></header>
 <!-- /wp:group -->
 
-<!-- wp:group {"style":{"layout":{"selfStretch":"fill","flexSize":null},"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-<div class="wp-block-group"><!-- wp:navigation {"ref":4,"submenuVisibility":"click","overlay":"navigation-overlay","icon":"menu","className":"is-style-priority-plus-navigation","style":{"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"small","layout":{"type":"flex","justifyContent":"right"},"priorityPlusEnabled":true,"priorityPlusToggleLabel":"Browse","priorityPlusToggleTextColor":"#C7DAF0","priorityPlusToggleTextColorHover":"#FFFFFF","priorityPlusMenuBackgroundColor":"#F5F3F0","priorityPlusMenuBorder":{"color":"#EEEBE6","style":"solid","width":"1px"},"priorityPlusMenuBoxShadow":"6px 6px 0px rgba(0, 0, 0, 0.2)","priorityPlusMenuItemHoverBackground":"#FFFFFF","priorityPlusMenuItemSeparator":{"style":"none","width":"0px"},"priorityPlusSubmenuBackgroundColor":"#FFFFFF","priorityPlusSubmenuItemHoverBackground":"#F5F3F0","priorityPlusTypographyFontSize":"small"} /-->
+<!-- wp:group {"metadata":{"name":"IDC Nav"},"className":"idc-site-header__nav","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group idc-site-header__nav"><!-- wp:navigation {"ref":4,"submenuVisibility":"click","overlay":"navigation-overlay","icon":"menu","className":"is-style-priority-plus-navigation","style":{"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"small","layout":{"type":"flex","justifyContent":"right"},"priorityPlusEnabled":true,"priorityPlusToggleLabel":"Browse","priorityPlusToggleTextColor":"#C7DAF0","priorityPlusToggleTextColorHover":"#FFFFFF","priorityPlusMenuBackgroundColor":"#F5F3F0","priorityPlusMenuBorder":{"color":"#EEEBE6","style":"solid","width":"1px"},"priorityPlusMenuBoxShadow":"6px 6px 0px rgba(0, 0, 0, 0.2)","priorityPlusMenuItemHoverBackground":"#FFFFFF","priorityPlusMenuItemSeparator":{"style":"none","width":"0px"},"priorityPlusSubmenuBackgroundColor":"#FFFFFF","priorityPlusSubmenuItemHoverBackground":"#F5F3F0","priorityPlusTypographyFontSize":"small"} /-->
 
-<!-- wp:buttons {"style":{"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-buttons">
-  <!-- wp:idc/language-toggle /-->
-</div>
+<!-- wp:buttons {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-buttons"><!-- wp:idc/language-toggle /--></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group --></section>
+<!-- /wp:group -->
+
+<!-- wp:group {"tagName":"section","metadata":{"name":"Contact Info"},"align":"full","className":"idc-site-header__small-print","layout":{"type":"constrained","contentSize":"1280px"}} -->
+<section class="wp-block-group alignfull idc-site-header__small-print"><!-- wp:paragraph {"align":"wide","style":{"typography":{"textAlign":"right"}}} -->
+<p class="has-text-align-right alignwide">Phone toll-free at <a href="tel:1-888-433-1011">1-888-433-1011</a> or email us at <a href="mailto:info@idocscanada.ca">info@idocscanada.ca</a></p>
+<!-- /wp:paragraph --></section>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
