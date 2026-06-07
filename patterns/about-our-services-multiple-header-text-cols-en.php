d87df0c1b190fea99549c41cad963598790c0f16
Troy Chaplin <troy.chaplin@gmail.com>
2026-06-07 19:34:04 -0400

    add new patterns

---
+++ patterns/about-our-services-multiple-header-text-cols-en.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Title: About Our Services: Multiple Header & Text Cols (EN)
+ * Slug: idc-block-theme/about-our-services-multiple-header-text-cols-en
+ * Categories: idc-sections-en
+ */
+?>
+<!-- wp:group {"tagName":"section","metadata":{"name":"About Our Services","categories":["idc-sections-en"]},"align":"full","className":"is-style-white idc-section","layout":{"type":"constrained"}} -->
+<section class="wp-block-group alignfull is-style-white idc-section"><!-- wp:group {"metadata":{"name":"Content Column"},"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"tagName":"header","metadata":{"name":"Section Heading: Left","patternName":"core/block/39656"},"align":"wide","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained","justifyContent":"left"}} -->
+<header class="wp-block-group alignwide" style="margin-bottom:var(--wp--preset--spacing--50)"><!-- wp:paragraph {"className":"has-thin-lines has-thin-lines\u002d\u002dright","style":{"typography":{"textAlign":"left"}}} -->
+<p class="has-text-align-left has-thin-lines has-thin-lines--right">About the Process</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"className":"has-gray-400-color has-text-color has-link-color","style":{"elements":{"link":{"color":{"text":"var:preset|color|grey-dark"}}},"typography":{"fontStyle":"normal","fontWeight":"400","textAlign":"left"}},"textColor":"grey-dark","fontSize":"heading-large"} -->
+<h2 class="wp-block-heading has-text-align-left has-gray-400-color has-text-color has-link-color has-grey-dark-color has-heading-large-font-size" style="font-style:normal;font-weight:400">Qatar Police<br><em><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-primary-light-color"><em><em>Clearance Certificate Process</em></em></mark></em></h2>
+<!-- /wp:heading --></header>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Content Column"},"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:paragraph {"align":"wide","fontSize":"medium"} -->
+<p class="alignwide has-medium-font-size">Getting your Police Clearance Certificate from Qatar can be straight forward. It can be ordered in person in Qatar, or you may apply through the Embassy of Qatar in Ottawa. However, you may only order your Police Clearance Certificate through the Embassy of Qatar if you are in Canada on a valid work or study permit.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Two Content Columns"},"align":"wide","className":"is-two-columns","style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide is-two-columns" style="margin-top:var(--wp--preset--spacing--40)"><!-- wp:group {"metadata":{"name":"Content Column"},"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size">If you were a resident in Qatar after 2012, or your residency ended after 2012, you should be exempt from having to prepare and submit a fingerprint form.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>If you were a resident in Qatar prior to 2012, you will also be asked to prepare and process a fingerprint form (C-216C). This form would require notarization, authentication, and legalization before it can be included in your Police Clearance Certificate application.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Content Column"},"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size"><strong>Requirements of the Embassy of Qatar:</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list {"fontSize":"medium"} -->
+<ul class="wp-block-list has-medium-font-size"><!-- wp:list-item -->
+<li>cover letter stating your request and the purpose</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>copy of the passport used in Qatar</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>copy of your old or expired residence visa in Qatar</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>copy of your current passport</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>copy of your current valid residence permit/citizenship in Canada (PRC, Work Permit)</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>four photographs, passport size (size: 4.8cm*3.8cm)</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Content Column"},"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|40","margin":{"top":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="margin-top:var(--wp--preset--spacing--60)"><!-- wp:group {"metadata":{"name":"Two Content Columns"},"align":"wide","className":"is-two-columns","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide is-two-columns"><!-- wp:group {"metadata":{"name":"Content Column"},"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"fontSize":"heading-medium"} -->
+<h2 class="wp-block-heading has-heading-medium-font-size">How We Can Help You</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size">We can assist with the document authentication and legalization process, or submit your Police Clearance Application to the Embassy of Qatar. We will work with you to handle as much of the process as you like.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size">Our friendly specialists would be happy to discuss the process and answer your questions. Phone us toll-free at&nbsp;<a href="tel:1-888-433-1011">1-888-433-1011</a>&nbsp;or use our <a href="https://idocs.local/get-in-touch/">contact form</a> to send us a quick online enquiry.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Content Column"},"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"fontSize":"heading-medium"} -->
+<h2 class="wp-block-heading has-heading-medium-font-size">About the Embassy of Qatar</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size"><strong>Hours of Operation</strong>:<br>Mon – Fri, 10am-12:00pm &amp; 2pm-3pm</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><strong>Address</strong>:<br>150 Metcalfe Street, 8th floor, Ottawa, Ontario, Canada, K2P 1P1</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><strong>Email:</strong> <a href="mailto:Ottawa@mofa.gov.qa">ottawa@mofa.gov.qa</a><br><strong>Phone number:</strong>&nbsp;<a href="tel:613-241-4917">613-241-4917</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size"><strong>Website:</strong> <a href="http://ottawa.embassy.qa/en/servicess/services">Embassy of Qatar</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></section>
+<!-- /wp:group -->
---
+++ patterns/about-our-services-multiple-header-text-cols-fr.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Title: About Our Services: Multiple Header & Text Cols (FR)
+ * Slug: idc-block-theme/about-our-services-multiple-header-text-cols-fr
+ * Categories: idc-sections-fr
+ */
+?>
+<!-- wp:group {"tagName":"section","metadata":{"name":"About Our Services","categories":["idc-sections-en"]},"align":"full","className":"is-style-white idc-section","layout":{"type":"constrained"}} -->
+<section class="wp-block-group alignfull is-style-white idc-section"><!-- wp:group {"metadata":{"name":"Content Column"},"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"tagName":"header","metadata":{"name":"Section Heading: Left","patternName":"core/block/39656"},"align":"wide","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained","justifyContent":"left"}} -->
+<header class="wp-block-group alignwide" style="margin-bottom:var(--wp--preset--spacing--50)"><!-- wp:paragraph {"className":"has-thin-lines has-thin-lines\u002d\u002dright","style":{"typography":{"textAlign":"left"}}} -->
+<p class="has-text-align-left has-thin-lines has-thin-lines--right">À propos de nos services</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"className":"has-gray-400-color has-text-color has-link-color","style":{"elements":{"link":{"color":{"text":"var:preset|color|grey-dark"}}},"typography":{"fontStyle":"normal","fontWeight":"400","textAlign":"left"}},"textColor":"grey-dark","fontSize":"heading-large"} -->
+<h2 class="wp-block-heading has-text-align-left has-gray-400-color has-text-color has-link-color has-grey-dark-color has-heading-large-font-size" style="font-style:normal;font-weight:400">Processus de demande<br><em><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-primary-light-color"><em><em>de certificat de police du Qatar</em></em></mark></em></h2>
+<!-- /wp:heading --></header>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Content Column"},"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:paragraph {"align":"wide","fontSize":"medium"} -->
+<p class="alignwide has-medium-font-size">Obtenir votre certificat de police du Qatar peut être simple. Il peut être demandé en personne au Qatar, ou vous pouvez faire une demande par l’intermédiaire de l’ambassade du Qatar à Ottawa. Cependant, vous ne pouvez demander votre certificat de police par l’ambassade du Qatar que si vous êtes au Canada avec un permis de travail ou d’études valide.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Two Content Columns"},"align":"wide","className":"is-two-columns","style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide is-two-columns" style="margin-top:var(--wp--preset--spacing--40)"><!-- wp:group {"metadata":{"name":"Content Column"},"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size">Si vous avez été résident au Qatar après 2012, ou si votre résidence s’est terminée après 2012, vous devriez être exempté de l’obligation de préparer et de soumettre un formulaire d’empreintes digitales.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Si vous avez été résident au Qatar avant 2012, vous devrez également préparer et traiter un formulaire d’empreintes digitales (C-216C). Ce formulaire devra être notarié, authentifié et légalisé avant de pouvoir être inclus dans votre demande de certificat de police.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Content Column"},"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size"><strong>Exigences de l’ambassade du Qatar :</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list {"fontSize":"medium"} -->
+<ul class="wp-block-list has-medium-font-size"><!-- wp:list-item -->
+<li>lettre d’accompagnement indiquant votre demande et son objectif</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>copie du passeport utilisé au Qatar</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>copie de votre ancien visa de résidence (expiré ou non) au Qatar</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>copie de votre passeport actuel</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>copie de votre permis de résidence actuel ou preuve de citoyenneté au Canada (carte de résident permanent, permis de travail)</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>quatre photos format passeport (taille : 4,8 cm x 3,8 cm)</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Content Column"},"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|40","margin":{"top":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="margin-top:var(--wp--preset--spacing--60)"><!-- wp:group {"metadata":{"name":"Two Content Columns"},"align":"wide","className":"is-two-columns","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide is-two-columns"><!-- wp:group {"metadata":{"name":"Content Column"},"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"fontSize":"heading-medium"} -->
+<h2 class="wp-block-heading has-heading-medium-font-size">Comment nous pouvons vous aider</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size">Nous pouvons vous aider avec le processus d’authentification et de légalisation de documents, ou soumettre votre demande de certificat de police à l’ambassade du Qatar. Nous travaillerons avec vous pour prendre en charge autant d’étapes du processus que vous le souhaitez.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size">Nos spécialistes sympathiques seront ravis de discuter du processus avec vous et de répondre à vos questions. Appelez-nous sans frais au <a href="tel:18884331011">1-888-433-1011</a> ou utilisez notre <a href="https://idocs.local/contactez-nous/?lang=fr">formulaire de contact</a> pour nous envoyer une demande rapide en ligne.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Content Column"},"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"fontSize":"heading-medium"} -->
+<h2 class="wp-block-heading has-heading-medium-font-size">À propos de l’ambassade du Qatar</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size"><strong>Heures d’ouverture :</strong><br>Lundi au vendredi, de 10 h à 12 h et de 14 h à 15 h</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><strong>Adresse :</strong><br>150, rue Metcalfe, 8e étage, Ottawa (Ontario), Canada, K2P 1P1</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><strong>Courriel</strong> <strong>:</strong> <a href="mailto:Ottawa@mofa.gov.qa">ottawa@mofa.gov.qa</a><br><strong>Téléphone</strong> <strong>:</strong>&nbsp;<a href="tel:613-241-4917">613-241-4917</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size"><strong>Site web</strong> <strong>:</strong> <a href="http://ottawa.embassy.qa/en/servicess/services">Embassy of Qatar</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></section>
+<!-- /wp:group -->
